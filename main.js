2aeabebcf065e853f374b4903ebec0da3d966790
hardikkala <84086962+hardikkala@users.noreply.github.com>
2022-01-20 19:41:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+}
+
+function play()
+{
+	song.play();
+}
+
+
+
+
+
+
+
